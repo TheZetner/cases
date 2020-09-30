631f9c56bf9c5dab82900b6476a900c4aa0024f6
Adrian Zetner <azetner@cscscience.ca>
2020-09-30 14:17:12 -0500

    modify install.R to rebuild binder version

--- install.R
+++ install.R
@@ -12,5 +12,6 @@
 # install.packages("shinydashboard")
 # install.packages("lubridate")
 # install.packages("dplyr")
+install.packages("DT")
 install.packages("remotes")
 remotes::install_github("TheZetner/cases")
